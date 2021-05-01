4af8d16b16ec990e102a4df7ea0eea9fc36e3e16
Bernardo Mota <bernardomota.est@gmail.com>
2021-05-01 11:19:57 -0300

    :zap: Add analysis of results

--- results/tables_and_graph.py
+++ results/tables_and_graph.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-path = 'results_1/dfs/'
+path = 'results/dfs/'
 files = os.listdir(path)
 
 f = files[0]
@@ -17,9 +17,27 @@ for id, f in enumerate(files[1:]):
     df_f1 = df_f1.drop(['metric name', 'step'], axis=1)
     df_rf[f'value{id + 2}'] = df_f1['value']
 col = df_rf.loc[:, 'value':'value5']
+df_rf['value_min'] = col.min(axis=1)
 df_rf['value_mean'] = col.mean(axis=1)
+df_rf['value_max'] = col.max(axis=1)
+df_rf['value_var'] = col.var(axis=1)
+df_rf = df_rf.drop(['value', 'value2', 'value3', 'value4', 'value5'], axis=1)
 
 train_size = df_rf['train size'].unique().tolist()
+
+# Tables
+df_entropy = df_rf[df_rf['classifier name'] == f'tripletnet + RF [entropy]']
+df_random = df_rf[df_rf['classifier name'] == f'tripletnet + RF [random]']
+df_top_two_margin = df_rf[df_rf['classifier name'] == f'tripletnet + RF']
+df_entropy = df_entropy.drop(['classifier name'], axis=1)
+df_random = df_random.drop(['classifier name'], axis=1)
+df_top_two_margin = df_top_two_margin.drop(['classifier name'], axis=1)
+
+df_entropy.round(4).to_csv('results/metrics_per_step/df_entropy.csv', index=False)
+df_random.round(4).to_csv('results/metrics_per_step/df_random.csv', index=False)
+df_top_two_margin.round(4).to_csv('results/metrics_per_step/df_top_two_margin.csv', index=False)
+
+# Graph
 entropy = df_rf['value_mean'][df_rf['classifier name'] == f'tripletnet + RF [entropy]'].tolist()
 random = df_rf['value_mean'][df_rf['classifier name'] == f'tripletnet + RF [random]'].tolist()
 top_two_margin = df_rf['value_mean'][df_rf['classifier name'] == f'tripletnet + RF'].tolist()
